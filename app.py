fc2920d3bc8da4d16a8e1a403244b46e50b0c00a
adrianjuicer <adrian42arthur@gmail.com>
2026-05-22 19:09:14 -0300

    Atualização

--- app.py
+++ app.py
@@ -2,13 +2,11 @@ from flask import Flask
 
 from config import ConfiguracaoProjeto
 from controllers.principal_controller import principal_blueprint
-from extensions import inicializar_extensoes
 
 
 app = Flask(__name__)
 app.config.from_object(ConfiguracaoProjeto)
 
-inicializar_extensoes(app)
 app.register_blueprint(principal_blueprint)
 
 
