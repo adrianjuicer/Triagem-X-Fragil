8a0eeb5f6ee98a0f91427ca24efb4c429b7e429d
adrianjuicer <adrian42arthur@gmail.com>
2026-05-22 18:56:57 -0300

    Correção de arquivo
    Mandei arquivo errado da primeira vez

--- app.py
+++ app.py
@@ -5,18 +5,11 @@ from controllers.principal_controller import principal_blueprint
 from extensions import inicializar_extensoes
 
 
-def criar_app():
-    """Cria a aplicacao Flask e registra a estrutura MVC inicial."""
-    app = Flask(__name__)
-    app.config.from_object(ConfiguracaoProjeto)
+app = Flask(__name__)
+app.config.from_object(ConfiguracaoProjeto)
 
-    inicializar_extensoes(app)
-    app.register_blueprint(principal_blueprint)
-
-    return app
-
-
-app = criar_app()
+inicializar_extensoes(app)
+app.register_blueprint(principal_blueprint)
 
 
 if __name__ == "__main__":
